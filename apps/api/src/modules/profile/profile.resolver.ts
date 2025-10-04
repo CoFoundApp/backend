028423cc3ea04bf64f0e406dd7e72e997824bcd5
MrBartou <anthony.denin@outlook.com>
2025-10-04 16:24:03 +0200

    feat: :sparkles: Add some change for deploy

--- apps/api/src/modules/profile/profile.resolver.ts
+++ apps/api/src/modules/profile/profile.resolver.ts
@@ -51,19 +51,17 @@ export class ProfileResolver {
   }
 
   /** Moi: lire mon profil (créé s'il n'existe pas) */
-  // @UseGuards(SessionGuard)
+  @UseGuards(SessionGuard)
   @Query(() => Profile, { description: 'Lecture de mon profil' })
-  async myProfile() {
-    let user = { sub: 'c7bcb8c3-b2f3-4fff-b20c-424dddea0235', role: 'USER' } as any; // TODO remove this line (test only)
+  async myProfile(@CurrentUser() user: JwtUser) {
     if (!user) throw new UnauthorizedException();
     return this.profiles.ensureMyProfile(user.sub);
   }
 
   /** Moi: mise à jour de mon profil */
-  // @UseGuards(SessionGuard)
+  @UseGuards(SessionGuard)
   @Mutation(() => Profile, { description: 'Mise à jour de mon profil' })
-  async updateMyProfile(@Args('input') input: UpdateMyProfileInput) {
-    let user = { sub: 'c7bcb8c3-b2f3-4fff-b20c-424dddea0235', role: 'USER' } as any; // TODO remove this line (test only)
+  async updateMyProfile(@CurrentUser() user: JwtUser, @Args('input') input: UpdateMyProfileInput) {
     if (!user) throw new UnauthorizedException();
     if (input.avatar) {
       input.avatar_url = await this.uploads.save(input.avatar);
